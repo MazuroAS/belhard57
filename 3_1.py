64f4b8643674e610a1a8aad8dd81e1ce41ad8735
MazuroAS <odd101510@gmail.com>
2022-07-01 11:36:28 +0300

    homework 3_1 is ready

--- 3_1.py
+++ 3_1.py
@@ -1,12 +1,18 @@
 #Пользователь вводит предложение, заменить все пробелы на "-" 2-мя  способами
 
+# метод 1 просто через  replace
 text: str = 'first friday'
 print(text.replace(' ','-'))
 
+
+# метод  просто через  цикл if c условием, что если нет ля замены ничего, нам оператор напечатает это
+text_2: str = 'firstfriday'
 a=' '
-if a in text:
-    text=text.replace
-print(text)
+if a in text_2:
+        print(text_2.replace(' ','-'))
+else:
+    print('нет символов для замены')
+
 
 
 
