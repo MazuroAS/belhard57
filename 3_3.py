6f4daa7a48c2679bbf1a9f7f96ca0a6caf75d095
MazuroAS <108143992+MazuroAS@users.noreply.github.com>
2022-07-01 18:29:38 +0300

    Update 3_3.py
    добавила # c помощью f-string

--- 3_3.py
+++ 3_3.py
@@ -8,10 +8,9 @@ city='Minsk'
 all_inf_0 = name + age + city
 print(all_inf_0)
 
-# c помощью %
-all_inf_1 = "Меня зовут %(n)s.Мне %(a)s лет. Я из города %(c)s.  % {"n": name, "a": age, "c": city}
-print(all_inf_1)
-
 #метод format()
 all_inf_2='Меня зовут {n}, Мне {a} лет. Я из города {c}.'.format(n=name,a=age,c=city)
 print(all_inf_2)
+
+# c помощью f-string
+print(f"Меня зовут, {name}. Мне {age} лет,я из города {city}.")
