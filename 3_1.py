a454b7a8f51d71dbc074edc0477f9735dee3a6e5
MazuroAS <odd101510@gmail.com>
2022-07-01 10:01:05 +0300

    homework 3_2 is ready

---
+++ 3_1.py
@@ -0,0 +1,13 @@
+#Пользователь вводит предложение, заменить все пробелы на "-" 2-мя  способами
+
+text: str = 'first friday'
+print(text.replace(' ','-'))
+
+a=' '
+if a in text:
+    text=text.replace
+print(text)
+
+
+
+
