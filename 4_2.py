5786ade1f1dd7d650ada63b1a61d3a47a6cc4b42
MazuroAS <odd101510@gmail.com>
2022-07-07 21:55:03 +0300

    добавила решение

--- 4_2.py
+++ 4_2.py
@@ -2,19 +2,20 @@
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
 
-
+# вариант 1
 def split(n):
     return [i for i in n]
 n = (input('введите любой текст с клавиатуры'))
 k = len(n)  # выводит число символов во введенном с клавы тексте
 print("кол-во символов, введенных с клавиатуры : ",k)
-
 print("введенный с клавиатуры текст: ", n)
-
 result = dict(zip(list(n),[list(n).count(i) for i in list(n)]))
 print("Dictionary :",result)
 
-
+# вариант 2
+text:  str = input('введите текст: ')
+counter: dict = {symbol: text.count(symbol) for symbol in text}
+print(counter)
 
 
 
--- 4_3.py
+++ 4_3.py
@@ -2,16 +2,20 @@
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
 
-n = int(input('введите n'))
-for b in range(n + 1):
-    ee = {'name': 0, 'email': 0}
-    cc = input('Введите name')
-    dd = input('Ведите email')
-    ee_2 = {'name': cc, 'email': dd}
-    b = b + 1
-    ee.update(ee_2)
-    print(ee)
+#n = int(input('введите n'))
+#for b in range(n + 1):
+    #ee = {'name': 0, 'email': 0}
+    #cc = input('Введите name')
+    #dd = input('Ведите email')
+    #ee_2 = {'name': cc, 'email': dd}
+    # = b + 1
+    #ee.update(ee_2)
+    #print(ee)
+    # додумать как закончить этот вариант
 
+n = int(input('введите n: '))
+users: dict = {i: {'name': input('name: '), 'email': input('email: ')} for i in range(n)}
+print(users)
 
 
 
