c512c0c587109c7a13ad481f8be79aa5953685ac
MazuroAS <108143992+MazuroAS@users.noreply.github.com>
2022-07-15 18:40:08 +0300

    Update 7_1.py

--- 7_1.py
+++ 7_1.py
@@ -5,3 +5,13 @@
 N = int(input('введите число: '))
 print("YES" if (N & (N - 1)) == 0 else "NO")
 
+
+# вариант , разобранный на уроке
+def is_step_two(N: int) -> bool:
+  if N !=1:
+    if not N % 2:
+      return is_step_two(N//2)
+    return False
+  else:
+    return True
+  print(is_step_two)
