7eaf70c70c7ade3b3241211a5afe6b8bc8493ca3
MazuroAS <108143992+MazuroAS@users.noreply.github.com>
2022-07-20 19:03:04 +0300

    Update 8.py

--- 8.py
+++ 8.py
@@ -19,3 +19,34 @@ def divisors_for_num(our_num):
 print(divisors_for_num(1522605027922533360535618378132637429718068114961380688657908494580122963258952897654000350692006139))
 
 
+numbers =[1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,4,4,4,5,5,5,5,5,]
+i=0
+while i<len(numbers):
+    for j in range(i+1,len(numbers)-1):
+        if numbers[i]==numbers[j]:
+            count+=1
+        else:
+            result.extend([numbers[i],count)
+            break
+        if numbers[i]==numbers[j]:
+            numbers.extend([numbers[i],count)
+     i=j
+print(result)
+                            
+                            
+  # бинарный поиск
+  numbers = [i for i in range(1,100)]
+a = 34
+i = len(numbers)//2
+count =1
+while numbers[i] !=a:
+    count += 1
+    if numbers[i]>a:
+        numbers = numbers[:i]
+    else:
+        numbers = numbers[i+1:]
+    i = len(numbers)//2
+print(i)
+                            
+                            
+
